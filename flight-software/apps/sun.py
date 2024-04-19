0c406f75e451908966a198867ae01ac10f9d6ffd
Ibrahima <ibrahima.sr.sow@gmail.com>
2024-04-19 15:58:58 -0400

    normalization sun vec

--- flight-software/apps/sun.py
+++ flight-software/apps/sun.py
@@ -1,10 +1,10 @@
 import math
 
 
-def process_sun_vec(raw_readings, prev_sun_vec):
+def process_sun_vector(raw_readings, prev_sun_vec):
 
     # TODO Should process the raw readings to determine the sun vector
-    angle = math.radians(0.001)  # Rotation angle
+    angle = math.radians(0.01)  # Rotation angle
 
     eclipse_state = in_eclipse(raw_readings)
 
@@ -13,6 +13,9 @@ def process_sun_vec(raw_readings, prev_sun_vec):
     new_y = prev_sun_vec[0] * math.sin(angle) + prev_sun_vec[1] * math.cos(angle)
     sun_vec = [new_x, new_y, prev_sun_vec[2]]
 
+    magnitude = math.sqrt(sum(component ** 2 for component in sun_vec))
+    sun_vec = [sun_vec[0] / magnitude, sun_vec[1] / magnitude, sun_vec[2] / magnitude]
+    
     return sun_vec, eclipse_state
 
 
