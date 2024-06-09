88a7a7188de5498fe1a3577d724e309662c567a6
ibrahima <ibrahima.sr.sow@gmail.com>
2024-06-09 03:51:34 -0400

    fix import

--- tests/cp_mock.py
+++ tests/cp_mock.py
@@ -2,6 +2,6 @@ import sys
 
 import numpy
 
-sys.path.append("tests/cp")
+sys.path.append("tests/cp/")
 sys.modules["micropython"] = __import__("micropython_mock")
 sys.modules["ulab"] = __import__("ulab_mock")
