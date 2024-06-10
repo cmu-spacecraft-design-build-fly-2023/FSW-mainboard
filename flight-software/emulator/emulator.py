fa86a4ded3290c5057fa3cfa05bb4a4b89d5f356
Thomas Damiani <thomas5d@comcast.net>
2024-06-10 11:55:33 -0400

    Revert "updates to main emulator file"
    This reverts commit 1fa92320762f269d54aa6007e180acedbd70ed81.
    
    Revert merging main, wanted to merge driver-updates

--- flight-software/emulator/emulator.py
+++ flight-software/emulator/emulator.py
@@ -1,14 +1,3 @@
-from numpy import array
-
-from emulator.cubesat import CubeSat
-from emulator.drivers.burnwire import BurnWires
-from emulator.drivers.radio import Radio
-from emulator.drivers.sd import SD
-from emulator.drivers.payload import Payload
-from emulator.drivers.sun_sensor import SunSensor
-from emulator.drivers.imu import IMU
-from emulator.drivers.power_monitor import PowerMonitor
-
 class device:
     """
     Based on the code from: https://docs.python.org/3/howto/descriptor.html#properties
@@ -34,37 +23,8 @@ class device:
             return self._device
 
 
-class satellite(CubeSat):
-    def __init__(self, enable_middleware, debug) -> None:
-        self.__middleware_enabled = enable_middleware
-        self.__debug = debug
+class satellite:
+    pass
 
-        super().__init__()
 
-        self.__burnwires = BurnWires()
-        self.__radio = Radio()
-        self.__sd_card = SD()
-        self.__payload_uart = Payload()
-
-        self.__vfs = None
-        self.__gps = None
-        self.__charger = None
-        
-        self.__sun_sensor_xp = SunSensor(900)
-        self.__sun_sensor_xm = SunSensor(48000)
-        self.__sun_sensor_yp = SunSensor(85000)
-        self.__sun_sensor_ym = SunSensor(200)
-        self.__sun_sensor_zp = SunSensor(12000)
-        self.__sun_sensor_zm = SunSensor(5000)
-
-        self.__torque_x = None
-        self.__torque_y = None
-        self.__torque_z = None
-
-        accel = array([1.0, 2.0, 3.0])
-        mag = array([4.0, 3.0, 1.0])
-        gyro = array([0.0, 0.0, 0.0])
-        self.__imu = IMU(accel=accel, mag=mag, gyro=gyro, temp=20)
-
-        self.__jetson_monitor = PowerMonitor(4, 0.05)
-        self.__battery_monitor = PowerMonitor(4.2, 0.04)
+SATELLITE = satellite()
