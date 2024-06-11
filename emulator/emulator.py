c6eede4db165191414e3c9540f4db0fcb11380fa
tomyyyD <thomas5d@comcast.net>
2024-06-11 11:38:21 -0400

    devices no longer private objects

--- emulator/emulator.py
+++ emulator/emulator.py
@@ -42,33 +42,33 @@ class satellite(CubeSat):
 
         super().__init__()
 
-        self.__burnwires = BurnWires()
-        self.__radio = Radio()
-        self.__sd_card = SD()
-        self.__payload_uart = Payload()
+        self._burnwires = BurnWires()
+        self._radio = Radio()
+        self._sd_card = SD()
+        self._payload_uart = Payload()
 
-        self.__vfs = None
-        self.__gps = None
-        self.__charger = None
-        
-        self.__sun_sensor_xp = SunSensor(900)
-        self.__sun_sensor_xm = SunSensor(48000)
-        self.__sun_sensor_yp = SunSensor(85000)
-        self.__sun_sensor_ym = SunSensor(200)
-        self.__sun_sensor_zp = SunSensor(12000)
-        self.__sun_sensor_zm = SunSensor(5000)
+        self._vfs = None
+        self._gps = None
+        self._charger = None
 
-        self.__torque_x = None
-        self.__torque_y = None
-        self.__torque_z = None
+        self._sun_sensor_xp = SunSensor(900)
+        self._sun_sensor_xm = SunSensor(48000)
+        self._sun_sensor_yp = SunSensor(85000)
+        self._sun_sensor_ym = SunSensor(200)
+        self._sun_sensor_zp = SunSensor(12000)
+        self._sun_sensor_zm = SunSensor(5000)
+
+        self._torque_x = None
+        self._torque_y = None
+        self._torque_z = None
 
         accel = array([1.0, 2.0, 3.0])
         mag = array([4.0, 3.0, 1.0])
         gyro = array([0.0, 0.0, 0.0])
-        self.__imu = IMU(accel=accel, mag=mag, gyro=gyro, temp=20)
+        self._imu = IMU(accel=accel, mag=mag, gyro=gyro, temp=20)
 
-        self.__jetson_monitor = PowerMonitor(4, 0.05)
-        self.__battery_monitor = PowerMonitor(4.2, 0.04)
+        self._jetson_monitor = PowerMonitor(4, 0.05)
+        self._battery_monitor = PowerMonitor(4.2, 0.04)
 
     def boot_sequence(self) -> list[int]:
         pass
