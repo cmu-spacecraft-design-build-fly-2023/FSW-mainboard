a6cd6b32853f8898459b4f056f5cfe6d447be91e
tomyyyD <thomas5d@comcast.net>
2024-06-12 13:44:42 -0400

    last linting update (hopefully)

--- emulator/cp/ulab_mock.py
+++ emulator/cp/ulab_mock.py
@@ -1,4 +1,1 @@
-import numpy
-
-# to get rid of Flake8 F401 because noqa comment isn't working
-numpy.abs(5)
+import numpy  # noqa: F401
--- emulator/cp_mock.py
+++ emulator/cp_mock.py
@@ -1,7 +1,5 @@
 import sys
 
-import numpy
-
 sys.path.append("./lib/hal/cp/")
 sys.modules["micropython"] = __import__("micropython_mock")
 sys.modules["ulab"] = __import__("ulab_mock")
