ef484eb1571266480ab052b2b82501ae660a7953
tomyyyD <thomas5d@comcast.net>
2024-06-19 11:18:22 -0400

    removing properties from rtc

--- flight/tasks/timing.py
+++ flight/tasks/timing.py
@@ -17,7 +17,7 @@ class Task(TemplateTask):
 
         if SM.current_state == "STARTUP":
             r = rtc.RTC()
-            r.datetime = time.struct_time((2024, 4, 24, 9, 30, 0, 3, 115, -1))
+            r.set_datetime(time.struct_time((2024, 4, 24, 9, 30, 0, 3, 115, -1)))
             # rtc.set_time_source(r)
         elif SM.current_state == "NOMINAL":
             print(
