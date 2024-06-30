5489000fa97bca325bda6633a3f34f401f75b384
ibrahima <ibrahima.sr.sow@gmail.com>
2024-06-30 17:18:45 +0200

    modify sun task

--- flight/apps/sun.py
+++ flight/apps/sun.py
@@ -43,20 +43,24 @@ def compute_body_sun_vector(I_vec):
 
     Args:
         I_vec: flux values on each face in the following order
-        - X+ face, X- face, Y+ face, Y- face, Z+ face, Z- face
+        - X+ face, X- face, Y+ face, Y- face, Z+ face
 
     Returns:
         sun_body: unit vector from spacecraft to sun expressed in body frame
     """
-    sun_body = [I_vec[0] - I_vec[1], I_vec[2] - I_vec[3], I_vec[4] - I_vec[5]]
+
+    status = False
+
+    sun_body = [I_vec[0] - I_vec[1], I_vec[2] - I_vec[3], I_vec[4]]
 
     norm = (sun_body[0] ** 2 + sun_body[1] ** 2 + sun_body[2] ** 2) ** 0.5
 
     # Normalize the vector if the norm is not zero
     if norm != 0:
         sun_body = [x / norm for x in sun_body]
+        status = True
 
-    return sun_body
+    return status, sun_body
 
 
 def in_eclipse(raw_readings):
--- flight/tasks/sun.py
+++ flight/tasks/sun.py
@@ -2,7 +2,7 @@
 
 import time
 
-from apps.sun import compute_body_sun_vector, in_eclipse
+from apps.sun import read_light_sensors, compute_body_sun_vector, in_eclipse
 from core import TemplateTask
 from core import state_manager as SM
 from core.data_handler import DataHandler as DH
@@ -14,9 +14,10 @@ class Task(TemplateTask):
     name = "SUN"
     ID = 0x11
 
-    data_keys = ["time", "x", "y", "z", "eclipse"]
+    data_keys = ["time", "status", "x", "y", "z", "eclipse"]
 
     # Fake starting sun vector
+    status = False
     sun_vector = [1, 0, 0]
     eclipse_state = False
 
@@ -26,24 +27,19 @@ class Task(TemplateTask):
 
             if not DH.data_process_exists("sun"):
                 DH.register_data_process(
-                    "sun", self.data_keys, "ffffb", True, line_limit=50
+                    "sun", self.data_keys, "fbfffb", True, line_limit=50
                 )
 
-            # TODO: Access Sun Sensor Readings - Satellite must return the array directly
-            lux_readings = [
-                SATELLITE.LIGHT_SENSOR_XP.lux(),
-                SATELLITE.LIGHT_SENSOR_XM.lux(),
-                SATELLITE.LIGHT_SENSOR_YP.lux(),
-                SATELLITE.LIGHT_SENSOR_YM.lux(),
-                SATELLITE.LIGHT_SENSOR_ZP.lux()
-            ]
+            # Access Sun Sensor Readings - Satellite must return the array directly
+            lux_readings = read_light_sensors()
 
-            # TODO Fake sun vector that always moves infinitesimally
-            self.sun_vector = compute_body_sun_vector(lux_readings)
+
+            self.status, self.sun_vector = compute_body_sun_vector(lux_readings)
             self.eclipse_state = in_eclipse(lux_readings)
 
             readings = {
                 "time": time.time(),
+                "status": self.status,
                 "x": self.sun_vector[0],
                 "y": self.sun_vector[1],
                 "z": self.sun_vector[2],
