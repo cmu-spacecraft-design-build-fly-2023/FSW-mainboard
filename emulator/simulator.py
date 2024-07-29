4d21d0ce41fb83edc00d6c7356f82950611a2c70
tomyyyD <thomas5d@comcast.net>
2024-07-29 15:09:23 -0400

    changing around imports?

--- emulator/simulator.py
+++ emulator/simulator.py
@@ -1,13 +1,13 @@
-from datetime import datetime, timedelta
+import datetime
 
-import numpy as np
+import numpy
 
 from simulation.argusloop.spacecraft import Spacecraft
 
 
 class Simulator:
     def __init__(self):
-        self.epoch_time = datetime.now()
+        self.epoch_time = datetime.datetime.now()
         self.dt = 1.0
         config = {
             "mass": 2.0,
@@ -22,7 +22,7 @@ class Simulator:
             "third_body": True,
         }
         self.time = self.epoch_time
-        self.u = np.zeros(3)
+        self.u = numpy.zeros(3)
 
         self.spacecraft = Spacecraft(config)
 
@@ -32,4 +32,4 @@ class Simulator:
         iters = int(secs / self.dt)
         for _ in range(iters):
             self.spacecraft.advance(self.u)
-        self.time += timedelta(seconds=(iters * self.dt))
+        self.time += datetime.timedelta(seconds=(iters * self.dt))
