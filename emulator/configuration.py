eadd1b64870a56804bb5c4364ab3c3b5af0cbf64
tomyyyD <thomas5d@comcast.net>
2024-07-29 13:25:29 -0400

    adding user input for settings and simulation setting

--- emulator/configuration.py
+++ emulator/configuration.py
@@ -2,9 +2,10 @@ from hal.cubesat import CubeSat
 from hal.emulator import satellite
 
 # Enable for Middleware
-DEBUG_MODE = True
-EN_MIDDLEWARE = True
-SOCKET_RADIO = True
+DEBUG_MODE = input("Enter debug mode? (y/n): ").lower().strip() == "y"
+EN_MIDDLEWARE = input("Enable middleware testing? (y/n): ").lower().strip() == "y"
+SOCKET_RADIO = input("Communicate with locally running groundstation? (y/n): ").lower().strip() == "y"
+SIMULATION = input("Use simulator data? (y/n): ").lower().strip() == "y"
 
 SATELLITE: CubeSat = None
-SATELLITE = satellite(enable_middleware=EN_MIDDLEWARE, debug=DEBUG_MODE, use_socket=SOCKET_RADIO)
+SATELLITE = satellite(enable_middleware=EN_MIDDLEWARE, debug=DEBUG_MODE, use_socket=SOCKET_RADIO, simulate=SIMULATION)
